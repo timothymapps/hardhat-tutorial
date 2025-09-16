ecde1216542367ecd1fb26d5ffba403d528f0eb7
Timothy Mendez Feliciano <timothym.apps@gmail.com>
2025-09-16 12:26:14 -0400

    super ready to teach

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,13 +8,18 @@ const INFURA_API_KEY = process.env.INFURA_API_KEY;
 const config: HardhatUserConfig = {
     solidity: "0.8.28",
 
-    // networks: {
-    //     hardhat: {
-    //         forking: {
-    //             url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
-    //         }
-    //     }
-    // }
+    networks: {
+        localhost: {
+            url: "http://127.0.0.1:8545"
+            // accounts: provided by the local node, no need to define
+        },
+        //     hardhat: {
+        //         forking: {
+        //             url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
+        //         }
+        //     }
+    }
 };
 
 export default config;
+
